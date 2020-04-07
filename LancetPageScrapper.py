94113d8b685eb2f7df4d47716ba4ed4e10686a23
martinKindall <mart256@gmail.com>
2020-04-07 14:18:16 -0400

    Moved comments.

--- LancetPageScrapper.py
+++ LancetPageScrapper.py
@@ -5,21 +5,11 @@ Created on Thu Apr  2 13:54:55 2020
 @author: Matias
 """
 
-# Función simple que scrappea la página https://www.thelancet.com/coronavirus para revisar si en los títulos
-# está la palabra entregada como parámetro y si está lo que se quiere buscar en el texto del paper, sirve como guia para empezar
-# un Scrapping más complejo
-# La función escribe un archivo .json con el título y el link de referencia del paper si contiene la palabra en el titulo
-# Requerimientos: pip install bs4, pip install requests.
-
 import webbrowser
 from bs4 import BeautifulSoup
 import requests
 import json
 from typing import List
-
-# Este método simplemente abre el paper si es que contiene la palabra buscada
-# Inputs: Url que contiene el título buscado, word_in_paper la palabra que se busca en paper
-# Output None
 from dataTypes.PaperData import PaperData
 from utils.PaperJsonEncoder import PaperJsonEncoder
 
@@ -29,21 +19,11 @@ from utils.PaperJsonEncoder import PaperJsonEncoder
         link = descarga.find('a')['href']
         webbrowser.open("https://www.thelancet.com" + link)
 """
-    
-# Función que scrappea la página principal de Lancet en búsqueda de palabras claves en el título
-# Palabra que se busca en titulo, palabra que se busca en el paper.
 
-def getPapersFromLancet() -> List[PaperData]:
-    url = "https://www.thelancet.com/coronavirus"
-    return getPapersFromUrl(url)
-
-def getLancetPapersOfInterest(word_in_title: str, word_in_paper: str) -> List[PaperData]:
-    paperArray = getPapersFromLancet()
-    return filterPapersOfInterest(
-        paperArray,
-        word_in_title,
-        word_in_paper
-    )
+# Función simple que scrappea la página https://www.thelancet.com/coronavirus para revisar si en los títulos
+# está la palabra entregada como parámetro y si está lo que se quiere buscar en el texto del paper, sirve como guia para empezar
+# un Scrapping más complejo
+# La función escribe un archivo .json con el título y el link de referencia del paper si contiene la palabra en el titulo
 
 def lancetScrappingAndOpenLinks(word_in_title: str, word_in_paper: str) -> None:
     paperArray = getPapersFromLancet()
@@ -53,7 +33,25 @@ def lancetScrappingAndOpenLinks(word_in_title: str, word_in_paper: str) -> None:
         word_in_title,
         word_in_paper
     )
-    for paper in papersOfInterest:
+    openPapersInBrowser(papersOfInterest)
+    
+def getPapersFromLancet() -> List[PaperData]:
+    url = "https://www.thelancet.com/coronavirus"
+    return getPapersFromUrl(url)
+
+# Función que scrappea la página principal de Lancet en búsqueda de palabras claves en el título
+# Palabra que se busca en titulo, palabra que se busca en el paper.
+
+def getLancetPapersOfInterest(word_in_title: str, word_in_paper: str) -> List[PaperData]:
+    paperArray = getPapersFromLancet()
+    return filterPapersOfInterest(
+        paperArray,
+        word_in_title,
+        word_in_paper
+    )
+
+def openPapersInBrowser(papers: List[PaperData]) -> None:
+    for paper in papers:
         print("El título del paper es: " + paper.title)
         webbrowser.open(paper.link)
 
