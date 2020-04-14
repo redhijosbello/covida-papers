d8d44959851e88f87bebcc2b9681cca32171e098
Jalbornozv <julio.albornoz@ug.uchile.cl>
2020-04-13 22:50:25 -0400

    Replaced selenium usage with bs4+requests on GoogleScholar
    This should allow integration with the GenericScraper class, while
    removing the need to use the chromedriver executable.

--- GoogleScholarScrapper/scrapping_google_scholar_beta.py
+++ GoogleScholarScrapper/scrapping_google_scholar_beta.py
@@ -1,15 +1,14 @@
 import pandas as pd
-from selenium import webdriver
 from typing import List
 from dataTypes.PaperData import PaperData
 from utils.PaperJsonEncoder import PaperJsonEncoder
 import json
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 import sys
-
+import bs4
+import os
+import requests
+import time
+import pdb
 
 class GoogleScholarScrapper():
     def clean_results(self,df_result):
@@ -25,44 +24,32 @@ class GoogleScholarScrapper():
         return df_result
 
     def getPapersFromUrl(self,num_pages, key_words) -> List[PaperData]:
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        if(sys.platform == 'linux'):
-            google_drive = "./chromedriver"
-        else:
-            google_drive = "C:\\Users\\Matias\\PycharmProjects\\covida-papers\\GoogleScholarScrapper\\chromedriver.exe"
-
-        driver = webdriver.Chrome(google_drive, chrome_options=options)
         link_to_search = "https://scholar.google.com/scholar?start=0&hl=es&as_sdt=0,5&q={0}".format(key_words)
         print(link_to_search)
-        driver.get(link_to_search)
         next_page = True
         page_num = 0
         stop = num_pages
         df_elements = pd.DataFrame({"name": [], "link": []})
         while next_page:
             link_to_ordered = link_to_search.replace("start=0", "start=" + str(page_num * 10))
-            wait = WebDriverWait(driver,20)
-            wait.until(EC.presence_of_element_located((By.ID,'gs_res_ccl_mid')))
-            search_list_paper = driver.find_elements_by_xpath('//*[@id="gs_res_ccl_mid"]//div//h3//a')
+            resp = requests.get(link_to_ordered)
+            page_soup = bs4.BeautifulSoup(resp.content, 'lxml')
+            time.sleep(20)
+            search_list_paper = [paper.h3.a for paper in page_soup.find(id='gs_res_ccl_mid').find_all('div') if paper.h3 is not None]
+            if search_list_paper == []: #No more results
+                next_page = False
+                break
+                
             for i in search_list_paper:
                 try:
-                    name = i.get_attribute('textContent')
-                    link = i.get_attribute('href')
+                    name = i.text
+                    link = i.attrs['href']
                     temp = pd.DataFrame({"name": [name], "link": [link]})
                     df_elements = df_elements.append(temp, ignore_index=True)
                 except:
                     continue
             page_num += 1
             df_results = self.clean_results(df_elements)
-            driver.get(link_to_ordered)
-            try:
-                driver.get(link_to_ordered)
-                next_page = True
-            except:
-                next_page = False
             if page_num >= stop:
                 next_page = False
         return self.getPapersFromDF(df_results)
